f5ce7d51e2b7c06165425195b490632a594f8dcd
Willie Smith <wsmith@catalystdevworks.com>
2016-12-02 18:12:47 -0500

    added Heroku config to server.js

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3003;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+      next();
+  }else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
 
-app.listen(3002, function(){
-  console.log('Express server is up on port 3002');
+app.listen(PORT, function(){
+  console.log('Express server is up on port ' + PORT);
 })
