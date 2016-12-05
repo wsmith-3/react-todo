e5b395ae0f50440709d5a0ac08b58086f20ad88c
Willie Smith <wsmith@catalystdevworks.com>
2016-12-04 21:47:38 -0500

    add todos reducer and test

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+import moment from 'moment';
+import uuid from 'node-uuid';
+
 export var searchTextReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
@@ -13,5 +16,35 @@ export var showCompletedReducer = (state = false, action) => {
       return !state
     default:
       return state
+  }
+}
+
+export var todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+        return [
+          ...state,
+          {
+            id: uuid(),
+            text: action.text,
+            completed: false,
+            createdAt: moment().unix(),
+            completedAt: undefined
+          }
+        ];
+    case 'TOGGLE_TODO':
+        return state.map((todo)=>{
+          if(todo.id === action.id){
+            var nextCompleted = !todo.completed;
+
+            return {
+              ...todo,
+              completed: nextCompleted,
+              completedAt: nextCompleted ? moment().unix() : undefined
+            }
+          }
+        })
+    default:
+      return state;
   }
 }
