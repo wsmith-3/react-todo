d450c19811641ea6ec4f9a031c43c4608a917036
Willie Smith <wsmith@catalystdevworks.com>
2016-12-05 20:47:11 -0500

    add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 
 import TodoApp from 'TodoApp';
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(()=>{
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load foundation-sites
 $(document).foundation();
 
---
+++ app/store/configureStore.jsx
@@ -0,0 +1,15 @@
+var redux = require('redux');
+import {searchTextReducer, showCompletedReducer, todosReducer} from 'reducers';
+
+export var configure = () => {
+  var reducer = redux.combineReducers({
+    searchText: searchTextReducer,
+    showCompleted: showCompletedReducer,
+    todos: todosReducer
+  });
+
+  var store = redux.createStore(reducer, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+  return store;
+};
